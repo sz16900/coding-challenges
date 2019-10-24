5677b3e706e96ed553dcdf486f5cf52ca436108f
sz16900 <sz16900@my.bristol.ac.uk>
2019-10-24 11:44:32 -0500

    advance challenges

--- scores.js
+++ scores.js
@@ -1,4 +1,4 @@
-function yeah(scores) {
+function breakingRecords(scores) {
     let x = 0;
     let y = 0;
     let a = scores[0];
@@ -8,14 +8,12 @@ function yeah(scores) {
             a = scores[i];
             x++;
         }
-    }
-    for (let i = 0; i < scores.length; i++) {
-        if (scores[i] < b) {
+        else if (scores[i] < b) {
             b = scores[i];
             y++;
         }
     }
-    console.log(`${x} ${y}`)
+    console.log(`${x} ${y}`);
 }
 
-console.log(yeah([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
