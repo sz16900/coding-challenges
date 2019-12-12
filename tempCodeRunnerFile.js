6e703bf11865c4c801c623ca2b8940061e5c21fa
sz16900 <sz16900@my.bristol.ac.uk>
2019-12-12 18:15:43 -0500

    testing githu

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+    // let string = s;
+    // let counter = 0;
+    // for (let i = 0; i < n; i++) {
+    //     if (string.length < n) {
+    //         string = string + s;
+    //     }
+    //     if (string[i] == "a") {
+    //         counter = counter + 1;
+    //     }
+
+    // }
+
+
+    // return counter;
