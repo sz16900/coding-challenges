8186741d2c22d2340207c75ff78e2fad0d471e58
sz16900 <sz16900@my.bristol.ac.uk>
2020-04-14 15:28:07 -0500

    transpositioning

---
+++ ruby/transposition.rb
@@ -0,0 +1,24 @@
+def transpose(str)
+    # your code here
+    cnt = 0
+    while cnt < str.length
+        if str[cnt] == 'g' && str[cnt+1] == 'n'
+            str[cnt], str[cnt+1] = str[cnt+1], str[cnt]
+             cnt = 0
+        end
+        cnt += 1
+    end
+    str.to_s
+end
+  
+  puts transpose('he was searchign on Bign for signign kigns')
+  # => he was searching on Bing for singing kings
+  
+  puts transpose('rignadingdiggn!')
+  # => ringadingdingg!
+  
+  puts transpose('gngngnnggnngggnnn')
+  # => nnnnnnnnngggggggg
+
+str = 'gngngnnggnngggnnn'
+str = str.split("")
