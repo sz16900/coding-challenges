a1097c858b94145464563ab3490518cd0e05f75a
sz16900 <sz16900@my.bristol.ac.uk>
2020-04-15 09:50:39 -0500

    :recycle: bonAppetit
    better performance

--- ruby/bonAppetit.rb
+++ ruby/bonAppetit.rb
@@ -1,10 +1,8 @@
-def bonAppetit(bill, k, b)
-    anaOwes = (bill.inject(:+) - bill[k]) / 2
-    if anaOwes != b
-        puts (bill.inject(:+) / 2) - anaOwes
-    else
-        puts "Bon Appetit"
-    end
+def bonAppetit(k, bill, b)
+    total_bill = bill.inject(:+) 
+    anaOwes = (total_bill - bill[k]) / 2  
+    return (total_bill / 2) - anaOwes if anaOwes != b
+    return "Bon Appetit"
 end
 
-bonAppetit([4, 1], [3, 10, 2, 9], 12)
+puts bonAppetit(1, [3, 10, 2, 9], 7)
